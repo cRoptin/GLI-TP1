28df441ddb55002e9abc13782bd7f1445a1cca6d
NicolasDuquesne <nicolas.duquesne@etudiant.univ-rennes1.fr>
2015-10-19 15:51:06 +0200

    La fenêtre

---
+++ appClientModule/fr/istic/gli/view/Fenetre.java
@@ -0,0 +1,81 @@
+package fr.istic.gli.view;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
+
+import fr.istic.gli.model.Camembert;
+import fr.istic.gli.model.Item;
+
+/**
+ * The Class Fenetre.
+ */
+public class Fenetre extends JFrame {
+		
+	/** The title. */
+	private JLabel title = new JLabel();
+	
+	/** The view. */
+	private View view;
+	
+	/** The model. */
+	final Camembert camembert;
+
+	/**
+	 * Instantiates a new fenetre.
+	 */
+	public Fenetre() {
+		this.setTitle("mon stock");
+		this.setSize(new Dimension(800, 600));
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Dessine le graphe représentant le stock
+		this.camembert = new Camembert();
+		this.camembert.addItem("livre", "", 40);
+		this.camembert.addItem("tasse", "", 10);
+		this.camembert.addItem("crayon", "", 50);
+		
+		title.setText(" ");
+		this.getContentPane().add(title, BorderLayout.SOUTH);
+
+		view = new View();
+		view.setCamembert(camembert);
+
+		this.getContentPane().add(view);
+
+		//this.getContentPane().add(new JScrollPane(camembert), BorderLayout.EAST);
+	}
+
+	/**
+	 * Gets the view.
+	 *
+	 * @return the view
+	 */
+	public View getView() {
+		return view;
+	}
+	
+	
+	/**
+	 * Handle mouse click.
+	 *
+	 * @param p the p
+	 */
+	public void handleMouseClick(Point2D p) {
+		/*int index = view.getArcPointClicked(p);
+		if (index != -1) {
+			title.setText("produit : " + model.getElements().get(index).getName()
+					+ " - " + " nombre en stock : " + model.getElements().get(index).getValue());
+			revalidate();
+		}*/
+	}
+
+
+}
